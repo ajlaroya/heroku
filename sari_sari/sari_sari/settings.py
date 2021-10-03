95bdd63235d699bcf3a61b20344d0e3e0b1fee71
ajlaroya <arthur.j.laroya@gmail.com>
2021-10-03 20:14:24 +1100

    test

--- sari_sari/sari_sari/settings.py
+++ sari_sari/sari_sari/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django_unused_media',
     'gunicorn',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -124,12 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = BASE_DIR / 'static'
 
 # Media files uploaded by user
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+MEDIA_URL = '/staticfiles/'
 
 # Log redirection
 LOGIN_REDIRECT_URL = '/' # redirection to home page
